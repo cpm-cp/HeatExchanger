d34cb68e857dbd2bc51ac87801160ac74953bea4
cpm-cp <cpallomendoza@gmail.com>
2024-02-19 22:49:07 -0500

    create Reynols class

---
+++ DimensionalessNumbers/Reynolds.py
@@ -0,0 +1,6 @@
+import numpy as np 
+
+
+class ReynoldNumber():
+    def __init__(self, velocity:float | None, diameter:float | None, radious:float | None, viscosity:float | None, ) -> None:
+        pass
