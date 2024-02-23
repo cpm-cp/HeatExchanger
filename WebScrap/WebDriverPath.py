9e9a9c395853a0c5e8d737c53fd91557cb75d9bb
cpm-cp <cpallomendoza@gmail.com>
2024-02-22 23:04:19 -0500

    update web path

--- WebScrap/WebDriverPath.py
+++ WebScrap/WebDriverPath.py
@@ -1,3 +1,3 @@
 WebDriverPath: str = r'C:\Users\Carlos Pallo\Downloads\edgedriver_win64\msedgedriver.exe'
 
-PATH: str = 'https://webbook.nist.gov/cgi/fluid.cgi?ID=C7732185&TUnit=K&PUnit=atm&DUnit=kg%2Fm3&HUnit=kJ%2Fmol&WUnit=m%2Fs&VisUnit=Pa*s&STUnit=N%2Fm&Type=IsoBar&RefState=DEF&Action=Page'
+PATH: str = 'https://webbook.nist.gov/cgi/fluid.cgi?ID=C7732185&TUnit=F&PUnit=atm&DUnit=lbm%2Fft3&HUnit=Btu%2Flbm&WUnit=m%2Fs&VisUnit=cP&STUnit=N%2Fm&Type=IsoBar&RefState=DEF&Action=Page'
