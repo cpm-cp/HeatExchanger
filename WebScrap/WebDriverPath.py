e64d8386e216e011a558b45fed40add63bb757f6
cpm-cp <cpallomendoza@gmail.com>
2024-02-24 02:02:55 -0500

    remove my WebDriverPath

--- WebScrap/WebDriverPath.py
+++ WebScrap/WebDriverPath.py
@@ -1,3 +1,1 @@
-WebDriverPath: str = r'C:\Users\Carlos Pallo\Downloads\edgedriver_win64\msedgedriver.exe'
-
 PATH: str = 'https://webbook.nist.gov/cgi/fluid.cgi?ID=C7732185&TUnit=F&PUnit=atm&DUnit=lbm%2Fft3&HUnit=Btu%2Flbm&WUnit=m%2Fs&VisUnit=cP&STUnit=N%2Fm&Type=IsoBar&RefState=DEF&Action=Page'
