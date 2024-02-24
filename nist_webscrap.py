fc665d22caac50fc3a2c0d4053c5c3a8da630b52
cpm-cp <cpallomendoza@gmail.com>
2024-02-24 02:01:30 -0500

    pre-finish

--- nist_webscrap.py
+++ nist_webscrap.py
@@ -4,21 +4,18 @@ import numpy as np
 
 # Water values
 sustance_1:str = 'water'
-low_water_T:float = 300.91 # K
-high_water_T:float = 315.99 # K
+low_water_T:float = 81.986 # °F
+high_water_T:float = 109.112 # °F
 water_T_props = mean_temperature(inlet_temperature=low_water_T,outlet_temperature=high_water_T)
-water_rho, water_K, water_mu = get_water_thermophysic_info(mean_Temperature=water_T_props)
-water_data = np.array([water_rho, water_K, water_mu])
+water_density, water_Cp, water_viscosity, water_conductivity = get_water_thermophysic_info(mean_Temperature=water_T_props)
+water_data = (water_density, water_Cp, water_viscosity, water_conductivity)
+print_properties('water', water_data, water_T_props)
 
 # Acetoen values
 acetone_density:float = 743.83 # kg/m3
 acetone_conductivity:float = 0.13955 # W/m*K
 acetone_viscosity:float = 0.37786 # cP;  1 cP = 0.001 Pa*s
 acetone_viscosity *= 0.001 # Pa*s
-low_acetone_T:float = 353.43 # K
-high_acetone_T:float = 366.41 # K
+low_acetone_T:float = 176.504 # °F
+high_acetone_T:float = 199.868 # °F
 acetone_T_props = mean_temperature(inlet_temperature=high_acetone_T, outlet_temperature=low_acetone_T)
-
-
-
-
