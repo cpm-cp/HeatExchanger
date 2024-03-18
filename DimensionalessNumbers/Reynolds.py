f6f06b67f1104d3613cc63f69fce3c8f7950aafe
cpm-cp <cpallomendoza@gmail.com>
2024-03-18 15:35:35 -0500

    Delete unnecessary folder

--- DimensionalessNumbers/Reynolds.py
+++
@@ -1,6 +0,0 @@
-import numpy as np 
-
-
-class ReynoldNumber():
-    def __init__(self, velocity:float | None, diameter:float | None, radious:float | None, viscosity:float | None, ) -> None:
-        pass
